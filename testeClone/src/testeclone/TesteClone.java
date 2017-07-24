38e322a534037a5ae8758688ba4274d24a697e9e
Jefferson Freire <j3ff1n@gmail.com>
2017-07-24 20:38:42 -0300

    Update TesteClone.java
    add nova linha de comando

--- testeClone/src/testeclone/TesteClone.java
+++ testeClone/src/testeclone/TesteClone.java
@@ -17,6 +17,7 @@ public class TesteClone {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("testeclone.TesteClone.main()");
+        System.out.println("Hello World");
     }
     
 }
