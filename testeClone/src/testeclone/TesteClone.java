11e4b2421cb0864961844bcfdd9e4542205f1d99
j3ff1n <j3ff1n@Jefferson>
2017-07-24 20:32:09 -0300

    testeagora4-240717

--- testeClone/src/testeclone/TesteClone.java
+++ testeClone/src/testeclone/TesteClone.java
@@ -16,6 +16,7 @@ public class TesteClone {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("testeclone.TesteClone.main()");
     }
     
 }
