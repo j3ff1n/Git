aa11828fd15c6afec980afc2972ffea822c2fdd7
Maxwel Melo <30329979+Maxfrutal@users.noreply.github.com>
2017-07-24 21:49:41 -0300

    Update TesteClone.java
    mudança feita pelo Maxwel

--- testeClone/src/testeclone/TesteClone.java
+++ testeClone/src/testeclone/TesteClone.java
@@ -19,6 +19,8 @@ public class TesteClone {
         System.out.println("testeclone.TesteClone.main()");
         System.out.println("Hello World");
         System.out.println("Mudança3");
+        System.out.println("Mudança Maxwel");
+        
     }
     
 }
