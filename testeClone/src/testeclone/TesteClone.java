6e45d74b3df90c100069d2606e74515df6f2f24d
Maxwel e Joice <Maxwel e Joice@MaxwelJoice>
2017-07-24 22:07:29 -0300

    Merge origin/master
    Conflicts:
    	testeClone/src/testeclone/TesteClone.java

--- testeClone/src/testeclone/TesteClone.java
+++ testeClone/src/testeclone/TesteClone.java
@@ -19,6 +19,8 @@ public class TesteClone {
         System.out.println("testeclone.TesteClone.main()");
         System.out.println("Hello World");
         System.out.println("ola mundo");
+        System.out.println("teste4");
+        
     }
     
 }
